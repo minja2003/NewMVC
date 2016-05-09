b01635f882089218321704155e7e367e2255310c
Minh Lam <minja2003@yahoo.com>
2016-05-09 17:09:07 -0400

    Final Update

---
+++ src/NewMVC/Models/StopsRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewMVC.Models
+{
+    public class StopsRepository
+    {
+        private TripContext dbContext;
+
+       
+        public StopsRepository(TripContext D)
+        {
+            dbContext = D;
+        }
+
+        public IEnumerable<Stop> Get(string tripN)
+        {
+            var trips = dbContext.Trips.Include(a => a.Stops).Where(a => a.Name == tripN).Single();
+            return trips.Stops;
+        }
+
+        public Trip AddTrip(Stop stop, Trip trip)
+        {
+            trip.Stops.Add(stop);
+            dbContext.Trips.Update(trip);
+            dbContext.SaveChanges();
+
+            return trip;
+        }
+    }
+}
